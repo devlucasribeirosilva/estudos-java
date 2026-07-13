20db606f373a7a331669feeb7c0c184a3df78ac2
Lucas <dev.lucasribeirosilva>
2026-07-13 20:40:37 -0300

    [loiane-basico] aula 5: Primeiro Programa em Java

---
+++ src/main/java/com/lucas/estudos/loiane/basico/OlaMundo.java
@@ -0,0 +1,14 @@
+package com.lucas.estudos.loiane.basico;
+
+/**
+ * 1 - Criar um arquivo OlaMundo.java
+ * 2 - implementar um programa java que escreve "Olá mundo!"
+ * 3 - Compilar e executar o programa
+ * **/
+
+public class OlaMundo {
+    public static void main(String[] args) {
+
+        System.out.println("Olá mundo!");
+    }
+}
